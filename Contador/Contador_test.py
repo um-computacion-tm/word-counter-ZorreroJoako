d5e749c1353cca90839f0a2723e4cfc54c755ca8
Joakito908 <j.zorrero@alumno.um.edu.ar>
2023-04-11 08:39:33 -0300

    Segundotp

---
+++ Contador/Contador_test.py
@@ -0,0 +1,20 @@
+import unittest
+from Contador import count_words 
+class TestCountWords(unittest.TestCase):
+
+    def test_simple(self):
+        result = count_words('hola')
+        self.assertEqual(result, {'hola': 1})
+
+    def test_complex(self):
+        result = count_words('Hola como estas hola')
+        self.assertEqual(
+            result,
+            {
+                'hola': 2,
+                'como': 1,
+                'estas': 1,
+            },
+        )
+if __name__ == '__main__':
+    unittest.main()
